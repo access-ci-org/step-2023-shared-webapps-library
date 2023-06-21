6a519751a12dc8ee709aeacf0249ff1ca70907bb
Nowshin Anber <117230814+nowshin-108@users.noreply.github.com>
2023-06-21 15:24:25 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <GoogleMap
+      :apiKey="apiKey"
+      :center="center"
+      :zoom="zoom"
+    >
+      <GoogleMapMarker
+        v-for="marker in markers"
+        :key="marker.id"
+        :position="marker.position"
+      />
+    </GoogleMap>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { GoogleMap, GoogleMapMarker } from 'vue3-google-map';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    GoogleMap,
+    GoogleMapMarker,
+  },
+  data() {
+    return {
+      apiKey: 'AIzaSyAhz8xQwJzSD3LBpW1mLFtZ204MPp8b4h4',
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+      markers: [],
+    };
+  },
+  mounted() {
+    this.getMarkers();
+  },
+  methods: {
+    async getMarkers() {
+      const response = await fetch(
+        'https://operations-api.access-ci.org/wh2/cider/v1/access-allocated/'
+      );
+      const data = await response.json();
+      const results = data.results;
+      this.markers = results.map((result) => ({
+        id: result.id,
+        position: { lat: result.latitude, lng: result.longitude },
+      }));
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
