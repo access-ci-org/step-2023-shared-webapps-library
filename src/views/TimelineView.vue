e383f3a60571344ede1d9f7f795c18b9dcec1db9
nowshin-108 <07nowshin.anber@gmail.com>
2023-07-28 03:25:10 +0600

    combined vue components

--- src/views/TimelineView.vue
+++ src/views/TimelineView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="timesheet-default">
+  <div id="timesheet">
 
   </div>
 </template>
@@ -9,21 +9,82 @@ import "timesheet.js/dist/timesheet.min";
 import "timesheet.js/dist/timesheet.min.css";
 import "timesheet.js/dist/timesheet-white.min.css";
 
+
+function compareFn(a, b) {
+  // Convert the latest_status_end fields to Date objects
+  const dateA = new Date(a[1].split('/').reverse().join('-'));
+  const dateB = new Date(b[1].split('/').reverse().join('-'));
+
+  // Compare the dates
+  if (dateA < dateB) {
+    return -1;
+  } else if (dateA > dateB) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+const words = ['lorem', 'dolor', 'default'];
+
 export default {
   mounted() {
-    new window.Timesheet('timesheet-default', 2002, 2013, [
-      ['2002', '09/2002', 'A freaking awesome time', 'lorem'],
-      ['06/2002', '09/2003', 'Some great memories', 'ipsum'],
-      ['2003', 'Had very bad luck'],
-      ['10/2003', '2006', 'At least had fun', 'dolor'],
-      ['02/2005', '05/2006', 'Enjoyed those times as well', 'ipsum'],
-      ['07/2005', '09/2005', 'Bad luck again', 'default'],
-      ['10/2005', '2008', 'For a long time nothing happened', 'dolor'],
-      ['01/2008', '05/2009', 'LOST Season #4', 'lorem'],
-      ['01/2009', '05/2009', 'LOST Season #4', 'lorem'],
-      ['02/2010', '05/2010', 'LOST Season #5', 'lorem'],
-      ['09/2008', '06/2010', 'FRINGE #1 & #2', 'ipsum']
-    ]);
+    // Fetch data from the API
+    fetch('https://operations-api.access-ci.org/wh2/cider/v1/access-allocated/')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Extract the fields from the data
+        const projects = data.results.map(project => ({
+          resource_descriptive_name: project.resource_descriptive_name,
+          latest_status_begin: project.latest_status_begin,
+          latest_status_end: project.latest_status_end
+        }));
+
+        // Create an array of timesheet data
+        const timesheetData = projects
+          .filter(project => project.latest_status_begin && project.latest_status_end)
+          .map(project => {
+            // Calculate the time difference between the end date and current date
+            const endDate = new Date(project.latest_status_end);
+            const currentDate = new Date();
+            const timeDiff = endDate.getTime() - currentDate.getTime();
+            const diffDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24)); // Convert to days
+
+            // Choose an element from the words array based on the time difference
+            let word;
+            if (diffDays <= 365) {
+              word = 'default';
+            } else if (diffDays <= 730) {
+              word = 'dolor';
+            } else {
+              word = 'lorem';
+            }
+
+            return [
+              project.latest_status_begin.slice(5, 7) + '/' + project.latest_status_begin.slice(0, 4),
+              project.latest_status_end.slice(5, 7) + '/' + project.latest_status_end.slice(0, 4),
+              project.resource_descriptive_name,
+              word
+            ];
+          });
+
+        timesheetData.sort(compareFn);
+
+        // Initialize the timesheet
+        new window.Timesheet('timesheet', 2012, 2050, timesheetData);
+        console.log(timesheetData)
+      });
   }
 };
 </script>
+<style scoped>
+  #timesheet {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
