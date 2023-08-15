b84c6c35f636e48b46d429250d216493f1c58e37
Dinuka De Silva <l.dinukadesilva@gmail.com>
2023-08-15 14:24:31 -0400

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const ASSET_URL = process.env.ASSET_URL || ''
+const env = loadEnv(
+  'mock',
+  process.cwd(),
+  ''
+);
+
+const ASSET_URL = env.ASSET_URL || '';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `${ASSET_URL}/dist/`,
+  base: `${ASSET_URL}`,
   plugins: [
     vue(),
   ],
